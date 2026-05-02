c9c77b2383d434e8cc26405e53554d0fb27f1f91
Samir Roy <samirroy.it2002@gmail.com>
2026-05-02 17:42:18 +0530

    done

--- server/src/controllers/BudgetController.ts
+++ server/src/controllers/BudgetController.ts
@@ -4,7 +4,7 @@ import budgetService from "../services/BudgetService";
 export const getBudget = async (req: Request, res: Response) => {
   const { month } = req.params;
   try {
-    const budget = await budgetService.getBudgetByMonth(month);
+    const budget = await budgetService.getBudgetByMonth(month as string);
     res.json(budget);
   } catch (err: any) {
     res.status(500).json({ error: err.message });
